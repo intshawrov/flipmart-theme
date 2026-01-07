70566456a582de3b09fd67221287cd57369e5c82
Shawrov <intshawrov@gmail.com>
2026-01-07 16:46:51 +0600

    Updated all files

--- header.php
+++ header.php
@@ -3,17 +3,10 @@
 	<head>
 		<!-- Meta -->
 		<meta charset="<?php bloginfo( 'charset' ); ?>">
-		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
-		<meta name="description" content="">
-		<meta name="author" content="">
-	    <meta name="keywords" content="MediaCenter, Template, eCommerce">
-	    <meta name="robots" content="all">
-
-	    <title><?php bloginfo( 'name' ); ?></title>
-
 		
 
+	    <title><?php bloginfo( 'name' ); ?> | <?php bloginfo( 'description' ); ?></title>
 		
 		<!-- Icons/Glyphs -->
 		<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/font-awesome.css">
