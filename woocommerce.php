98716ff7b652504c82bae96be0250fbff4f990bc
Shawrov <intshawrov@gmail.com>
2026-01-07 17:21:39 +0600

    Add all files

--- woocommerce.php
+++ woocommerce.php
@@ -18,10 +18,7 @@
 			<div class="blog-page">
 				<div class="col-md-9">
 					<div class="blog-post  wow fadeInUp">
-	<?php while ( have_posts() ) : the_post(); ?>
-	<h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
-	<?php the_content(); ?>
-	<?php endwhile; ?>
+	<?php woocommerce_content(); ?>
 	
 </div>
 
