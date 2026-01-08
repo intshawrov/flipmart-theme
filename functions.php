641d54c7a3ae95e961c63bec121a4393681ea0e4
Shawrov <intshawrov@gmail.com>
2026-01-08 16:35:28 +0600

    Updated

--- functions.php
+++ functions.php
@@ -115,3 +115,8 @@ add_action( 'init', 'woo_remove_wc_breadcrumbs' );
 function woo_remove_wc_breadcrumbs() {
     remove_action( 'woocommerce_before_main_content', 'woocommerce_breadcrumb', 20, 0 );
 }
+
+add_action( 'init', 'flipmart_remove_wc_breadcrumbs' );
+function flipmart_remove_wc_breadcrumbs() {
+    remove_action( 'woocommerce_before_shop_loop', 'woocommerce_result_count', 20, 0 );
+}
